fd557fcae411fa4305ddf90213456a4470a75bbc
5x <longhorn482653@gmail.com>
2023-02-26 17:02:55 +0800

    feat(gatsby-config.js): add link preview plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -57,6 +57,7 @@ module.exports = {
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
           `gatsby-remark-smartypants`,
+          `gatsby-remark-link-preview`,
         ],
       },
     },
