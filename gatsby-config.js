a7a61d83d067b243472d7f3aa2ba18f14a448326
5x <longhorn482653@gmail.com>
2023-07-12 21:10:41 +0800

    feat(gatsby-config.js): migrate to GA4

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,7 +20,7 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         // The property ID; the tracking code won't be generated without it
-        trackingId: `UA-158905919-1`,
+        trackingId: `G-MB8J3CFYQL`,
       },
     },
     {
